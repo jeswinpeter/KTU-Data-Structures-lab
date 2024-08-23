39d0fe916a00e10f695b6c87fbf438661dedd9b0
jeswinpeter <147077261+jeswinpeter@users.noreply.github.com>
2024-08-23 21:36:33 +0530

    Add files via upload

---
+++ selection.c
@@ -0,0 +1,69 @@
+//This code performs Selection sort on an array and calculates time and space complexity
+#include<stdio.h>
+void main(){
+	int size;		//Size of the array
+ 	int i;			//Variable incremented in while loop
+ 	int j;			//Variable incremented in while loop
+ 	int temp;		//Variable used to swap 2 array elements
+	int a[30];		//Array used to take values
+	int time = 0;	//Variable to track time complexity of the code
+	int space = 0;	//Variable to track space complexity of the code
+	int small;		//Index of smallest element in the portion of the array that is being sorted 
+	time += 2;
+
+ printf("Enter size of array: ");
+ scanf("%d",&size);
+ time += 2;
+
+//loop to take values to the array
+ printf("Enter the elements: \n");
+	for (i = 0; i < size; i++) {
+ 		scanf("%d",&a[i]);
+		time += 2;
+ 	}
+ time += 2;
+
+//loop to print unsorted array
+ printf("The unsorted array is : \n");
+ 	for (i = 0; i < size; i++) {
+ 		printf("%d\t",a[i]);
+		time += 2;
+ 	}
+	time += 2;
+
+//Selection sort loops
+ 	i = 0;
+ 	while(i < size) {
+ 		j = i + 1;
+ 		small = i;
+ 		while(j < size) {
+ 			if(a[small] > a[j]) {
+ 				small = j;
+				time ++;
+ 			}
+			j++;
+			time += 2;
+ 		}
+ 		if(i != small) {
+ 			temp = a[small];
+ 			a[small] = a[i];
+ 			a[i] = temp;
+			time += 4;
+ 		}
+ 		i++;
+		time += 4;
+ 	}
+	time += 2;
+
+//loop to print unsorted array
+	printf("\nThe sorted array is : \n");
+ 	for (i = 0; i < size; i++) {
+ 		printf("%d\t",a[i]);
+		time += 2;
+ 	}
+ 	printf("\n");
+
+	time += 5;
+	space = (7 * 4) + (size * 4);	// Space complexity calculation
+ 	printf("Space complexity = %d\nTime complexity = %d\n",space,time);
+ }
