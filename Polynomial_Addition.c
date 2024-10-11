10549ac553b5db955c560addc2b1eaf86aa62466
jeswin <jeswinpeter92@gmail.com>
2024-10-11 23:46:45 +0530

    Halfway there

--- Polynomial_Addition.c
+++ Polynomial_Addition.c
@@ -10,22 +10,41 @@ struct node {
 struct node * head_p = NULL;
 struct node * head_q = NULL;
 struct node * head_r = NULL;
-struct node * p_ptr,* q_ptr,* r_ptr;
+struct node * ptr,* p_ptr,* q_ptr,* r_ptr;
 struct node * Ctemp;			//Used for temporary storage of address 
 struct node * tempH;			//Used for temporary storage of address
 int value;
 int count;
+int no_terms_1;	//No of terms in the 1st Polynomial
+int no_terms_2;	//No of terms in the 2nd Polynomial
+
+struct node * create_poly(int size,struct node * head) {
+	count = 0;
+	while(count < size) {
+		
+		ptr = (struct node *)malloc(sizeof(struct node));
+				printf("Enter exponent of X : ");
+				scanf("%d",&value);
+				ptr -> expo = value;
+				printf("Enter the coefficient of X^%d : ",value);
+				scanf("%d",&value);
+				ptr -> expo = value;
+				count++;
+				if(count = size - 1) {
+					ptr -> link = NULL;
+				}
+			}
+}
 
 /*create() -> to create new nodes
   Whenever a new node is created this function returns address of new node*/
 struct node * create() {
-	struct node *temp = (struct node *)malloc(sizeof(struct node));
+	
 	return temp;
 }
 
 int main() {
-	int no_terms_1;	//No of terms in the 1st Polynomial
-	int no_terms_2;	//No of terms in the 2nd Polynomial
+	
 	
 	printf("Enter no of terms in the 1st polynomial: ");
  	scanf("%d",&no_terms_1);
@@ -33,27 +52,10 @@ int main() {
  	scanf("%d",&no_terms_2);
 
 	//Loop to construct the Polynomial 
-	while(count < no_terms_1) {
-		Ctemp = create();
-		
-		if( Ctemp == NULL ) {
-			printf("!!! Memory Full !!!");
-			return 0;
-		}
-		else {
-			head_p = Ctemp;
-			p_ptr = head_p;
-			count = 0;
-			while(count < no_terms_1) {
-				printf("Enter exponent of X : ");
-				scanf("%d",&value);
-				p_ptr -> expo = value;
-				printf("Enter the coefficient of X^%d : ",value);
-				scanf("%d",&value);
-				p_ptr -> expo = value;
-				p_ptr = p_ptr -> link;
-				count++;
-			}
-		}
-	}
 }
+	
+		
+		
+		
+			
+			
